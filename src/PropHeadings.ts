70d7eab4bad67a304e712b6208d3952b86c5e556
igorskyflyer <igor.dvlpr@gmail.com>
2023-04-01 23:16:07 +0200

    Convert Astro -> TypeScript

---
+++ src/PropHeadings.ts
@@ -0,0 +1,8 @@
+export enum PropHeadings {
+  /** Leave the headings intact. */
+  Intact,
+  /** Remove Markdown code but keep the headings text. */
+  TextOnly,
+  /** Remove the headings completely. */
+  None,
+}
