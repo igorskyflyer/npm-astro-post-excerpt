7481cc93715f6b2b262db59c542f6c4857c07661
igorskyflyer <igor.dvlpr@gmail.com>
2023-04-05 23:14:20 +0200

    Remove and inline

--- src/PropRenderer.ts
+++
@@ -1,8 +0,0 @@
-export enum PropRenderer {
-  /** Render post excerpts as simple text, no HTML entities supported. */
-  Text,
-  /** Render post excerpts as HTML, HTML entities supported. */
-  Html,
-  /** Render post excerpts as simple text but allow HTML entities as well. */
-  Hybrid,
-}
