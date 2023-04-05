5771b59eb68929f0c8f1e50780594ab6357e01a6
igorskyflyer <igor.dvlpr@gmail.com>
2023-04-05 02:08:17 +0200

    Update JSDocs

--- src/PropRenderer.ts
+++ src/PropRenderer.ts
@@ -1,5 +1,5 @@
 export enum PropRenderer {
-  /** Render post excerpts as simple text,  no entities supported. */
+  /** Render post excerpts as simple text, no entities supported. */
   text,
   /** Render post excerpts as HTML, entities supported. */
   html,
